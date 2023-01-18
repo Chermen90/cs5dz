0874e664715d94823c360c85c02aacbbd026ab5d
st202222 <zarina.kochisova@gmail.com>
2023-01-17 23:13:09 -0500

    updated 34

---
+++ 34/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+Write("Введите размер массива: ");
+int length = int.Parse(ReadLine()!);
+int[] arr =GetArr(length);
+PrintArray(arr);
+WriteLine();
+WriteLine($"Количество чётных элементов в массиве равно {CountEven(arr)}");
+
+//метод для создания массива
+int[] GetArr(int size)
+{
+
+    int [] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] =  new Random().Next(100, 999);
+    }
+
+    return array;
+}
+
+//метод для печати массива
+void PrintArray(int[] inArray)
+{
+    for(int i = 0; i < inArray.Length-1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length-1]}");
+}
+
+int CountEven(int[]arCount)
+{
+    int result = 0;
+    foreach(int a in arCount)
+    {
+        if(a%2 ==0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
